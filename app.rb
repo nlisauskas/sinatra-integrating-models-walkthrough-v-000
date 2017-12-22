d665cc0e204f4b5058f72b82846fbf519b873f3e
Nick Lisauskas <nlisausk@alumni.nd.edu>
2017-12-22 23:00:38 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -5,11 +5,9 @@ class App < Sinatra::Base
   get '/' do
     erb :index
   end
-
+  
   post '/' do
-    text_from_user = params[:user_text]
-
-    @analyzed_text = TextAnalyzer.new(text_from_user)
+    @analyzed_text = TextAnalyzer.new(params[:user_text])
 
     erb :results
   end
